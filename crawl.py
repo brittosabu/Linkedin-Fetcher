6e6f510f50786446408430d3dca2bd1bdf1ccf72
brittosabu <britto.da3@iiitmk.ac.in>
2021-06-20 12:24:42 +0530

    get job details & navigate through pages

--- crawl.py
+++ crawl.py
@@ -13,7 +13,9 @@ class Linkedin:
         self.jobname = job
         self.location = location
         self.encrypt = encrypt
+        self.number_of_pages_to_fetch = 10
         
+        self.job_details = {}
         self.creds = {}
         self.read_creds()
         
@@ -69,21 +71,68 @@ class Linkedin:
         print("Job Searched")
         
     
-    def getting_job_details(self):
-        """
-        """
-        all_jobs = self.driver.find_element_by_xpath('//*[@class="jobs-search-results__list list-style-none"]')
-        jobs_list  = all_jobs.find_elements_by_tag_name("li")
+    def scroll_and_get_all_jobs(self):
+    
+        total_jobs_found_in_page  = 0
+        all_jobs = self.driver.find_elements_by_xpath('//div[@class="flex-grow-1 artdeco-entity-lockup__content ember-view"]')
         
+        while total_jobs_found_in_page!=len(all_jobs):
+            
+            total_jobs_found_in_page = len(all_jobs)
+            all_jobs[-1].click()
+            all_jobs = self.driver.find_elements_by_xpath('//div[@class="flex-grow-1 artdeco-entity-lockup__content ember-view"]')
+            
+        return all_jobs
+        
+    def get_all_job_details(self,all_jobs,page):
+    
+        for i,job in enumerate(all_jobs):
+            
+            i = (page*100)+i
+            
+            time.sleep(5)
+            print(job.text)
+            job.click()
+            self.job_details[i] = {}
+            
+            job_brief = job.text.split("\n")
+            self.job_details[i]["Position"] = job_brief[0]
+            self.job_details[i]["Company"] = job_brief[1]
+            self.job_details[i]["Location"] = job_brief[2]
+            
+            job_d = self.driver.find_element_by_xpath('//*[@id="job-details"]')
+            description = job_d.find_element_by_xpath('.//span[*]').text
+            
+            self.job_details[i]['JD'] = description
+            
+    def go_to_next_page(self):
+    
+        current_page = int(self.driver.find_element_by_xpath('//button[contains(@aria-current,"true")]').text)
+        next_p = self.driver.find_element_by_xpath('//button[@aria-label="Page {}"]'.format(str(current_page+1)))
+        next_p.click()
+        
+        return current_page
+
     def run(self):
         
         self.Login()
         self.search_jobs()
 
+        time.sleep(10)
+
+        current_p = 0
+
+        while current_p < self.number_of_pages_to_fetch:
+
+            all_jobs = self.scroll_and_get_all_jobs()
+            self.get_all_job_details(all_jobs,current_p)
+            current_p = self.go_to_next_page()
+
+        print(self.job_details)
 
 def main():
 
-    L = Linkedin("Data Scientist",'India')
+    L = Linkedin("Data Scientist",'India',encrypt=False)
     L.run()
 
 if __name__ == "__main__":
